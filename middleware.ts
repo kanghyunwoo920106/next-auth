d7ae566d5e98d79c61880c0b2eb72973534e4617
hyunoo <xkaizew123@gmail.com>
2025-01-06 11:49:58 +0900

    feat: middleware 추가

---
+++ middleware.ts
@@ -0,0 +1,20 @@
+import { NextResponse } from "next/server";
+import { getToken } from "next-auth/jwt";
+
+export default async function middleware(req) {
+  const token = await getToken({
+    req,
+    secret: process.env.NEXTAUTH_SECRET,
+  });
+
+  if (!token) {
+    return NextResponse.redirect(new URL("/auth", req.url));
+  }
+
+  return NextResponse.next();
+}
+
+// 인증 필요한 페이지 설정
+export const config = {
+  matcher: ["/", "/main/:path"],
+};
